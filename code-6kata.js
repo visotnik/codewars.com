07712432e78313f7bf739266be30824d5583b3b1
visotnik <tet129@gmail.com>
2022-02-13 11:59:32 +0200

    words reversed by condition

--- code-6kata.js
+++ code-6kata.js
@@ -73,3 +73,28 @@ function dividedBy(rightNum) {
 }
 
 console.log("===>", five(dividedBy(zero())));
+
+// =================================================================================
+// Write a function that takes in a string of one or more words, and returns
+// the same string, but with all five or more letter words reversed (Just like the name
+// of this Kata). Strings passed in will consist of only letters and spaces.
+// Spaces will be included only when more than one word is present.
+// Examples:
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+// spinWords( "This is a test") => returns "This is a test"
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(string) {
+  //TODO Have fun :)
+  const wordsArr = string.split(" ");
+  const spinArr = wordsArr.map((el) => {
+    if (el.length > 4) {
+      const elArr = el.split("");
+      return elArr.reverse().join("");
+    }
+    return el;
+  });
+  return spinArr.join(" ");
+}
+
+console.log("===> spinWords =", spinWords("This is another test"));
