f96de5865691f510a06def9be51ad81eccb43a5f
visotnik <tet129@gmail.com>
2022-02-13 11:30:01 +0200

    mathematic calculations using functions

---
+++ code-6kata.js
@@ -0,0 +1,75 @@
+// =================================================================================
+// This time we want to write calculations using functions and get the results.
+// Let's have a look at some examples:
+
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+
+// Each calculation consist of exactly one operation and two numbers
+// Division should be integer division. For example, this should return 2, not 2.666666...:
+
+// https://dev.to/aspittel/comment/6goi
+// https://www.cyberforum.ru/javascript-beginners/thread2614753.html
+
+// interesting solving
+// ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+// .forEach(function (name, n) {
+//     this[name] = function (f) { return f ? f(n) : n }
+//   });
+
+function zero(fn) {
+  return fn ? fn(0) : 0;
+}
+function one(fn) {
+  return fn ? fn(1) : 1;
+}
+function two(fn) {
+  return fn ? fn(2) : 2;
+}
+function three(fn) {
+  return fn ? fn(3) : 3;
+}
+function four(fn) {
+  return fn ? fn(4) : 4;
+}
+function five(fn) {
+  return fn ? fn(5) : 5;
+}
+function six(fn) {
+  return fn ? fn(6) : 6;
+}
+function seven(fn) {
+  return fn ? fn(7) : 7;
+}
+function eight(fn) {
+  return fn ? fn(8) : 8;
+}
+function nine(fn) {
+  return fn ? fn(9) : 9;
+}
+
+function plus(rightNum) {
+  return function (leftNum) {
+    return leftNum + rightNum;
+  };
+}
+function minus(rightNum) {
+  return function (leftNum) {
+    return Math.floor(leftNum - rightNum);
+  };
+}
+function times(numFn) {
+  return function (leftNum) {
+    return Math.floor(leftNum * rightNum);
+  };
+}
+function dividedBy(rightNum) {
+  return function (leftNum) {
+    if (rightNum === 0) return "на ноль делить нельзя";
+    return Math.floor(leftNum / rightNum);
+  };
+}
+
+console.log("===>", five(dividedBy(zero())));
--- codewars.js
+++ codewars.js
@@ -349,3 +349,28 @@ function solution(string) {
 }
 
 console.log("====> Camel Case =", solution("myFavoriteString"));
+
+// =================================================================================
+// Write an algorithm that takes an array and moves all of the zeros to the end,
+// preserving the order of the other elements.
+// Напишите алгоритм, который берет массив и перемещает все нули в конец,
+// сохраняя порядок остальных элементов.
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+var moveZeros = function (arr) {
+  // TODO: Program me
+  let newArr = [];
+  const zeroArr = arr.filter((el) => {
+    if (el !== 0) {
+      newArr.push(el);
+    }
+    return el === 0;
+  });
+  newArr = [...newArr, ...zeroArr];
+  return newArr;
+};
+
+console.log(
+  "===> All of the zeros to the end =",
+  moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"])
+);
